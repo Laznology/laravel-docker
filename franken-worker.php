b9dda2f6513703da2137a8b42cc8a3f3ba262b7e
LazTech <laznology.dev@gmail.com>
2025-09-29 11:43:08 +0700

    Add franken-worker.php to initialize environment variables for production

---
+++ franken-worker.php
@@ -0,0 +1,8 @@
+<?php
+$_ENV['APP_ENV'] = 'production';
+$_ENV['APP_BASE_PATH'] = __DIR__ . '/..';
+$_ENV['APP_PUBLIC_PATH'] = __DIR__;
+$_ENV['LARAVEL_OCTANE'] = 1;
+$_ENV['MAX_REQUESTS'] = 1000;
+$_ENV['REQUEST_MAX_EXECUTION_TIME'] = 30;
+require __DIR__.'/../vendor/laravel/octane/bin/frankenphp-worker.php';
